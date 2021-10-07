b3106684bcb5a427e30cedde830a1c129bbd099e
FML21 <90581521+FML21@users.noreply.github.com>
2021-10-07 15:35:04 +0200

    Changed a lot lol

--- Bas/Jaar 1/Week 3/wk3ex1.py
+++ Bas/Jaar 1/Week 3/wk3ex1.py
@@ -41,4 +41,72 @@ def spiral(initial_length, angle, multiplier):
         left(angle)
         initial_length *= multiplier
         spiral(initial_length, angle, multiplier)
+
+def chai(size):
+    """Our chai function!"""
+    if size < 5: 
+        return
+    else:
+        forward(size)
+        left(90)
+        forward(size/2)
+        right(90)
+        chai(size / 2)
+        right(90)
+        forward(size)
+        left(90)
+        chai(size / 2)
+        left(90)
+        forward(size/2.0)
+        right(90)
+        backward(size)
+        return
+
+def svtree(trunklength, levels):
+    """svtree: draws a side-view tree
+       trunklength = the length of the first line drawn ("the trunk")
+       levels = the depth of recursion to which it continues branching
+    """
+    if levels == 0:
+        return
+    else:
+        forward(trunklength)
+        left(30)
+        svtree(trunklength/2,levels-1)
+        right(60)
+        svtree(trunklength/2,levels-1)
+        left(30)
+        backward(trunklength)
+
+def snowflake(sidelength, levels):
+    """Fractal snowflake function, complete.
+       sidelength: pixels in the largest-scale triangle side
+       levels: the number of recursive levels in each side
+    """
+    flakeside(sidelength, levels)
+    left(120)
+    flakeside(sidelength, levels)
+    left(120)
+    flakeside(sidelength, levels)
+    left(120)
+
+def flakeside(sidelength, levels):
+    """flakeside is part of the function snowflake to make a third of its side
+
+    sidelength: the total lenght of one side
+    levels: the amount of times you want it to make spikes
+    """
+    if levels == 0:
+        forward(sidelength)
+    else:
+        flakeside(sidelength/3, levels-1)
+        right(60)
+        flakeside(sidelength/3, levels-1)
+        left(120)
+        flakeside(sidelength/3, levels-1)
+        right(60)
+        flakeside(sidelength/3, levels-1)
+
+def reset():
+    resetscreen()
         
