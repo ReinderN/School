21496ca5e64eeaa201c1403f3725a66d528336bc
FML21 <90581521+FML21@users.noreply.github.com>
2021-10-08 10:32:51 +0200

    edited wk3ex2.py

--- Bas/Jaar 1/Programmeren/Week 3/wk3ex2.py
+++ Bas/Jaar 1/Programmeren/Week 3/wk3ex2.py
@@ -1,9 +1,9 @@
 # Programmeren I, week 3 opgave 2
 # Bestandsnaam: wk3ex2.py
-# Naam:
+# Naam: Bas Mellens
 # Probleemomschrijving: Slaapwandelende student
 
-import random  
+from random import *
 
 
 def rs():
@@ -11,7 +11,31 @@ def rs():
        note that a call to rs() requires parentheses
        arguments: none at all!
     """
-    return random.choice([-1, 1])
+    return choice([-1, 1])
 
 def rwpos(start, nsteps):
-   
+    """rwpos gives the position of the sleepwalker after nsteps random."""
+    if nsteps < 0:
+        return 0
+    else:
+        start = start + rs()
+        rwpos(start, nsteps-1)
+        print("start is: ", start)
+
+def rwsteps(start, low, hi):
+    """rwpsteps needs a starting postion and then takes a random amount of steps between low and high
+    
+    low: the minimum amount of steps you want the bot to take
+    high: the maximum amount of steps you want the bot to take
+    
+    returns the amount of steps the bot has taken
+    """
+    if low < 0:
+        return 0
+    else:
+        steps = randint(low, hi)
+        step = rs()
+        start += step
+        print("|","_"*(5+step), "😴", "_"*(5-step),"|")
+        steps -= 1
+        return 1 + rwsteps(start, steps, steps)
