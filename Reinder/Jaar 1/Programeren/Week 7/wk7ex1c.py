7b14b4d22ab90aeb57c2d8df2ef6b726d12f8905
ReinderN <85369967+ReinderN@users.noreply.github.com>
2021-11-22 23:09:56 +0100

    Bliep Bloep
    Robots dit some work
    
    Co-Authored-By: FML21 <90581521+FML21@users.noreply.github.com>
    Co-Authored-By: ScarlettSpeedie <57591556+ScarlettSpeedie@users.noreply.github.com>

--- Reinder/Jaar 1/Programeren/Week 7/wk7ex1c.py
+++ Reinder/Jaar 1/Programeren/Week 7/wk7ex1c.py
@@ -24,7 +24,12 @@ def unique(L):
     elif ...    # check whether L[0] re-appears
     else ...    # otherwise...
     """
-    return False     # dummycode
+    if len(L) == 0:
+        return True
+    elif L[0] in L[1:]:
+        return False
+    else:
+        return unique(L[1:])
 
 
 def test(s):
@@ -36,11 +41,111 @@ def test(s):
     s = s.strip()                 # haal alle spaties aan het begin en eind van s weg
     list_of_strings = s.split()   # splits s op elke spatie en nieuwe regel
     # print("list_of_strings is", list_of_string)
-    list_of_integers = [int(s) for s in list_of_strings]  # converteer ze allemaal naar ints
+    # converteer ze allemaal naar ints
+    list_of_integers = [int(s) for s in list_of_strings]
     # print("list_of_integers is", list_of_integers)
     return unique(list_of_integers)
 
 
 # Uitproberen!
-result = test(NUMBERS)
+result = test('''91
+20
+29
+18
+87
+36
+65
+74
+63
+32
+81
+10
+19
+8
+77
+26
+55
+64
+53
+22
+71
+0
+9
+98
+67
+16
+45
+54
+43
+12
+61
+90
+99
+88
+57
+6
+35
+44
+33
+2
+51
+80
+89
+78
+47
+96
+25
+34
+23
+92
+41
+70
+79
+68
+37
+86
+15
+24
+13
+82
+31
+60
+69
+58
+27
+76
+5
+14
+3
+72
+21
+50
+59
+48
+17
+66
+95
+4
+93
+62
+11
+40
+49
+38
+7
+56
+85
+94
+83
+52
+1
+30
+39
+28
+97
+46
+75
+84
+73
+42''') # gedaan met a=21, c=9, m=100, seed=42 && N=100
 print("\nTest op uniekheid:  Het resultaat is", result)
