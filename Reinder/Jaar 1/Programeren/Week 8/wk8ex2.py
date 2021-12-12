51f6958962b59ae05e468225a506c20c0d01c569
ReinderN <85369967+ReinderN@users.noreply.github.com>
2021-12-12 22:53:11 +0100

    Als je dit leest ben je leuk <3
    Co-authored-by: ScarlettSpeedie <ScarlettSpeedie@users.noreply.github.com>
    Co-authored-by: FML21 <FML21@users.noreply.github.com>

--- Reinder/Jaar 1/Programeren/Week 8/wk8ex2.py
+++ Reinder/Jaar 1/Programeren/Week 8/wk8ex2.py
@@ -1,5 +1,9 @@
+# Bestandsnaam: wk8ex2.py
+# Naam: Bas Mallens, Reinder Noormans, Freek van Witzenburg
+
 # count_evens uit List-2
 def count_evens(nums):
+    """Geeft alle even getallen terug uit list-2"""
     total = 0
     for x in nums:
         if x % 2 == 0:
@@ -10,6 +14,7 @@ def count_evens(nums):
 
 
 def sum13(nums):
+    """" Telt alle getallen uit list 2. """
     sum = 0
     next = False
     for x in nums:
@@ -27,6 +32,7 @@ def sum13(nums):
 
 
 def big_diff(nums):
+    """Geeft de grootste veschril tussen 2 getallen terug uit list 2. """
     biggest = 0
     smallest = 999999
     for x in nums:
@@ -41,6 +47,7 @@ def big_diff(nums):
 
 
 def sum67(nums):
+    """Geeft alle getallen uit list 2 weer."""
     sum = 0
     geweest = False
     for x in nums:
@@ -58,6 +65,7 @@ def sum67(nums):
 
 
 def centered_average(nums):
+    """Geeft een mediaan van de getallen uit list 2 weer."""
     biggest = 0
     smallest = 999999
     for x in nums:
@@ -72,6 +80,7 @@ def centered_average(nums):
 
 
 def has22(nums):
+    """Bij een lijst waar 2x een 2 achter elkaar zit zal de functie true geven. Bij een lijst waar dat niet voorkomt false."""
     last = 0
     two2 = False
     for x in nums:
@@ -85,7 +94,37 @@ def has22(nums):
 
 
 def double_char(str):
+    """Verdubbelt elke letter uit een string"""
     dblchr = ''
     for x in str:
         dblchr += x + x
     return dblchr
+
+
+assert count_evens([4, 123, 2132, 345, 456]) == 3
+assert count_evens([2314, 234, 456, 1235, 332, 436]) == 5
+assert count_evens([196, 27634, 5433, 345, 1843, 324]) == 3
+
+assert sum13([4, 123, 2132, 345, 456]) == 3060
+assert sum13([2314, 234, 456, 1235, 332, 436]) == 5007
+assert sum13([196, 27634, 5433, 345, 1843, 324]) == 35775
+
+assert big_diff([4, 123, 2132, 345, 456]) == 2128
+assert big_diff([2314, 234, 456, 1235, 332, 436]) == 2080
+assert big_diff([196, 27634, 5433, 345, 1843, 324]) == 27438
+
+assert sum67([4, 123, 2132, 345, 456]) == 3060
+assert sum67([2314, 234, 456, 1235, 332, 436]) == 5007
+assert sum67([196, 27634, 5433, 345, 1843, 324]) == 35775
+
+assert centered_average([4, 123, 2132, 345, 456]) == 308
+assert centered_average([2314, 234, 456, 1235, 332, 436]) == 614
+assert centered_average([196, 27634, 5433, 345, 1843, 324]) == 1986
+
+assert has22([4, 123, 2132, 345, 456]) == False
+assert has22([2314, 2, 2, 1235, 22, 436]) == True
+assert has22([196, 27634, 5433, 345, 1843, 324]) == False
+
+assert double_char('abc') == 'aabbcc'
+assert double_char('AaBbCc') == 'AAaaBBbbCCcc'
+assert double_char('Bas Stinkt') == 'BBaass  SSttiinnkktt'
--- Reinder/Jaar 1/Programeren/Week 9/wk9ex1/wk9ex1.py
+++ Reinder/Jaar 1/Programeren/Week 9/wk9ex1/wk9ex1.py
@@ -1,19 +1,24 @@
 #
 # wk9ex1.py - Practicum Game of Life
 #
-# Naam:
+# Naam: BL.P.R. Noordmans, B. Mellens, F.M. van Witzenburg
 #
 
 import random
 
 
 def create_one_row(width):
-    """ returns one row of zeros of width "width"...  
+    """ returns one row of zeros of width "width"...
         You might use this in your create_board(width, height) function """
     row = []
     for col in range(width):
         row += [0]
     return row
+
+
+assert create_one_row(5) == [0, 0, 0, 0, 0]
+assert create_one_row(2) == [0, 0]
+assert create_one_row(7) == [0, 0, 0, 0, 0, 0, 0]
 
 
 def create_board(width, height):
@@ -23,6 +28,14 @@ def create_board(width, height):
         # gebruik de bovenstaande functie zodat ... één rij is!!
         a += [create_one_row(width)]
     return a
+
+
+assert create_board(5, 5) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+assert create_board(7, 4) == [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+assert create_board(8, 8) == [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 
 
 def print_board(a):
@@ -50,8 +63,16 @@ def diagonalize(width, height):
     return a
 
 
-def inner_cells(width, height):
+assert diagonalize(5, 5) == [[0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [
+    0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0]]
+assert diagonalize(7, 4) == [[0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [
+    0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+assert diagonalize(8, 8) == [[0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0], [
+    0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+def inner_cells(width, height):
+    """ Deze functie geeft de totale levende cellen weer die zich in de tweedimensionale array bevinden."""
     a = create_board(width, height)
 
     for row in range(1, height - 1):
@@ -60,11 +81,120 @@ def inner_cells(width, height):
     return a
 
 
-def random_cells(width, height):
+assert inner_cells(5, 5) == [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [
+    0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+assert inner_cells(7, 4) == [[0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 0], [
+    0, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]]
+assert inner_cells(8, 8) == [[0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [
+    0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+def random_cells(width, height):
+    """ Deze functie geeft het aantal random cellen weer dei 1 en 0 terug geeft."""
     a = create_board(width, height)
 
     for row in range(1, height - 1):
         for col in range(1, width - 1):
             a[row][col] = random.choice([0, 1])
     return a
+
+
+def copy(a):
+    """Returns a DEEP copy of the 2D array a."""
+    height = len(a)
+    width = len(a[0])
+    new_a = create_board(width, height)
+
+    for row in range(1, height - 1):
+        for col in range(1, width - 1):
+            new_a[row][col] = a[row][col]
+    return new_a
+
+
+assert copy([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [
+    0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [
+        0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+assert copy([[0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [
+    0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [
+        0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+assert copy([[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [
+        0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+def inner_reverse(a):
+    """ Deze functie geeft zorgt ervoor dat de buitenste randen geven aan de buitenkant 
+    van de array altijd 0 terug geven. """
+    height = len(a)
+    width = len(a[0])
+    reverse_a = create_board(width, height)
+    reversed_sa = 0
+
+    for row in range(1, height - 1):
+        for col in range(1, width - 1):
+            if a[row][col] == 0:
+                reversed_sa = 1
+            else:
+                reversed_sa = 0
+            reverse_a[row][col] = reversed_sa
+    return reverse_a
+
+
+assert inner_reverse([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [
+    0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [
+        0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+assert inner_reverse([[0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [
+    0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 0], [
+        0, 1, 0, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]]
+assert inner_reverse([[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [
+        0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+def count_neighbours(row, col, a):
+    """ Deze functie telt alle levende buren die van een cel aanwezig zijn. """
+    living_neighbours = 0
+
+    for nrow in range(-1, 2):
+        for ncol in range(-1, 2):
+            if nrow == 0 and ncol == 0:
+                living_neighbours += 0
+            else:
+                if a[row + nrow][col + ncol] == 1:
+                    living_neighbours += 1
+    return living_neighbours
+
+
+assert count_neighbours(1, 2, [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [
+    0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]) == 5
+assert count_neighbours(2, 4, [[0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [
+    0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]) == 0
+assert count_neighbours(3, 5, [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [
+    0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]) == 0
+
+
+def next_life_generation(a):
+    """Makes a copy of a and then advances one
+       generation of Conway's Game of Life within
+       the * inner cells * of that copy.
+       The outer edge always stays at 0.
+    """
+    new_a = copy(a)
+    height = len(a)
+    width = len(a[0])
+
+    for row in range(1, height - 1):
+        for col in range(1, width - 1):
+            decision = count_neighbours(row, col, a)
+            if decision < 2 or decision > 3:
+                new_a[row][col] = 0
+            elif decision == 3:
+                new_a[row][col] = 1
+            else:
+                new_a[row][col] = a[row][col]
+    return new_a
+
+
+assert next_life_generation([[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [
+    0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [
+        0, 1, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
